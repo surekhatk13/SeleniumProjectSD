1bc1cfa5110b9989aa1a9b0485322c797694c266
Doddi <surekhatk13@gmail.com>
2021-12-15 11:24:01 -0500

    Added orderPage

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+	String itemname;
+	float price;
+	
+	public void itemdescription()
+	{
+		System.out.println("The item is");
+	}
+	
+	
+}
